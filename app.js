d063c72acaab19a3ad2d3d42e86d6c7edfa4291c
GavonC <99353848+GavonC@users.noreply.github.com>
2023-11-13 11:21:24 -0600

    Update app.js

--- app.js
+++ app.js
@@ -39,3 +39,30 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://gavonC:<Password>@cluster0.t8myehj.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
