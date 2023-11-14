6c3297a1a6439bfec9250b75e2d377ddbc16f469
GavonC <99353848+GavonC@users.noreply.github.com>
2023-11-14 11:48:13 -0600

    Update tool.js

--- controllers/tool.js
+++ controllers/tool.js
@@ -15,8 +15,20 @@ exports.tool_list = async function(req, res)
 exports.tool_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: tool create post');
 };
-exports.tool_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: tool create post');
+exports.tool_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new tool();
+    document.tool_type = req.body.tool_type;
+    document.cost=req.body.cost;
+    document.size=req.body.size;
+    try{
+        let result = await document.save();
+        res.send(result);
+    }
+    catch(err){
+        res.status(500);
+        res.send('{"error":${err}}');
+    }
 };
 exports.tool_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: costume delete DELETE ' + req.params.id);
